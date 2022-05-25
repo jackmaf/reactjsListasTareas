91cc38e2c14428d1f4b1f466da769629f8cd881e
jackson florez jimenez <jackmaf0@gmail.com>
2022-05-25 15:57:33 -0500

    implementacion react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 // Cargas de librerias
 import React from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 // Carga de componentes hijos
 import { IndexListaTareas } from './components/lista_tareas/IndexListaTareas';
+import { IndexContacto } from './components/contacto/IndexContacto';
 // Carga de otros elementos (CSS, IMAGES)
+
+function ContactoPrueba(){
+  return (
+    <div>
+      <h1>Entraste al contacto</h1>
+      <IndexContacto/>      
+    </div>
+  );
+}
 
 function App() {
   return (
-    <IndexListaTareas/>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/">Lista de tareas</Link>
+        </li>
+        <li>
+          <Link to="/contacto">Contacto</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<IndexListaTareas/>}/>
+        <Route path="/contacto" element={<ContactoPrueba/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/contacto/IndexContacto.js
@@ -0,0 +1,10 @@
+// Cargas de librerias
+import React from 'react';
+// Carga de componentes hijos
+// Carga de otros elementos (CSS, IMAGES)
+
+function IndexContacto(){
+  return (<h1>Estoy en contacto</h1>);
+}
+
+export { IndexContacto };
