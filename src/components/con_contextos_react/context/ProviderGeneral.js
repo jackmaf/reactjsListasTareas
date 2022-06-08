d67890d2d86c3dcecb95ede12b812762b3bb59fc
jackson florez jimenez <jackmaf0@gmail.com>
2022-06-08 10:00:57 -0500

    a

--- src/components/con_contextos_react/context/ProviderGeneral.js
+++ src/components/con_contextos_react/context/ProviderGeneral.js
@@ -4,7 +4,7 @@ import React, {useState, createContext} from 'react';
 // Genera, crea o inicializa el contexto donde existiran los datos
 const FamiliaContexto = createContext();
 
-function ProviderGeneral(props) {
+const ProviderGeneral = (props) => {
   // DATOS
   const [bisabuelo, setBisabuelo]= useState("Max");
   const [abuelo, setAbuelo] = useState("Neils");
@@ -13,11 +13,11 @@ function ProviderGeneral(props) {
   const [hermano, setHermano] = useState("Uldren");
   // FamiliaContexto tiene Provider y Consumer en el atributo value
   // se colocan todos los datos que se pueden compartir entre los componentes suscritos
-  return (
-    <FamiliaContexto.Provider value={{
+  return(
+    <FamiliaContexto.Provider 
+      value={{ 
         bisabuelo, abuelo, padre, hermana, hermano,
-        setBisabuelo, setAbuelo, setPadre, setHermana, setHermano
-      }}>
+        setBisabuelo, setAbuelo, setPadre, setHermana, setHermano}}>
       {props.children}
     </FamiliaContexto.Provider>
   );
